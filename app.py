c8c8dae885604a07b16bf4900f8990117913b6ec
emmanoel-amunik <emmanoelamunik@hotmail.com>
2024-02-09 22:15:55 -0300

    01. Manipulacao de Strings

---
+++ app.py
@@ -0,0 +1,10 @@
+print("Sabor Express\n")
+
+print("1. Register restaurant")
+print("2. List restaurant")
+print("3. Activate restaurant")
+print("4. Exit\n")
+
+chosen_option = input("Choose an option: ")
+print(f"You chose the option {chosen_option}")
+
